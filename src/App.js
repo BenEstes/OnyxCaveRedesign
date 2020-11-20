0e46f722472d89a9f5e01120522e0461f7fc0266
Ben Estes <benestes@MacBook-Pro-4.local>
2020-11-20 09:42:40 -0600

    fixed navbar collapse issue, added some media query to the home page.

--- src/App.js
+++ src/App.js
@@ -50,9 +50,7 @@ export default function App() {
       <div>
       <nav className="navbar navbar-expand-lg navbar-dark onyx-dark">
             <a className="navbar-brand">Onyx Cave</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <Link className="nav-link" to=''>Home<span className="sr-only">(current)</span></Link>
