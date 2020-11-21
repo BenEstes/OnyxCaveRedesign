e56c513b5c7fac447c2fb1b66e5e28592a7ce5e5
Ben Estes <benestes@MacBook-Pro-4.local>
2020-11-20 23:55:55 -0600

    Nothing Productive was done...

--- src/components/CaveInfoCard.js
+++ src/components/CaveInfoCard.js
@@ -1,16 +1,16 @@
 function CaveInfoCard(props) {
     return (
-        <div class="flip-card col">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <img src={props.cardImage} alt="Avatar" style={{width:200+ 'px', height:300+'px'}} />
-                </div>
-                <div class="flip-card-back">
-                    <h1>{props.cardName}</h1>
-                    <p>{props.cardDescription}</p>
+            <div class="flip-card col">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src={props.cardImage} alt="Cave Formation" style={{ width: 200 + 'px', height: 300 + 'px' }} />
+                    </div>
+                    <div class="flip-card-back">
+                        <h1>{props.cardName}</h1>
+                        <p>{props.cardDescription}</p>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
